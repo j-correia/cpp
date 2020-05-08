78684257855284bbc63a990a957c612bba78d986
j-correia <mr.joao.correia@gmail.com>
2020-05-08 05:11:45 +0100

    My first cpp

--- myfirst.cpp
+++ myfirst.cpp
@@ -1,11 +1,11 @@
 // myfirst.ccp -- displays a message
-#include <iostream>
+#include <iostream>							// A preprecessor directive
 
-int main()
-{
-  using namespace std;
-  cout << "Come up and C++ me some time.";
-  cout << endl;
-  cout << "You won't regret it!" << endl;
-  return 0;
-}
+int main()									// Function header
+{											// Start of body Function
+  using namespace std;						// Make definitions visible
+  cout << "Come up and C++ me some time.";	// Message
+  cout << endl;								// Start a new line
+  cout << "You won't regret it!" << endl;	// More output
+  return 0;									// Terminate main()
+}											// End of function body
